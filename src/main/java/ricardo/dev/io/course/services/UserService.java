1d5e7af5b3529a8ed12cd0cde25feb046bdfe502
ricardoazev <ricardometronome@gmail.com>
2024-04-02 08:40:54 -0300

    Entidade Pedido. Datas com Instant e padrão ISO 8601 - PARTE 2

--- src/main/java/ricardo/dev/io/course/services/UserService.java
+++ src/main/java/ricardo/dev/io/course/services/UserService.java
@@ -11,14 +11,14 @@ import ricardo.dev.io.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
